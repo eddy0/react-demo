0968cc1563c57e77e398c9e2a86445f1aff91922
eddy0 <awindy369@gmail.com>
2018-03-13 21:26:24 -0700

    webpack setting
    a webpack4 setting for bootstrap & scss

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Layout from './layout'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './layout.scss'
+
+
+const app = document.getElementById('root')
+ReactDOM.render(<Layout />, app)
---
+++ src/layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Layout extends React.Component {
+    render() {
+
+        return(
+            <div>
+                <p>here</p>
+            </div>
+        )
+    }
+}
+
+export default Layout
